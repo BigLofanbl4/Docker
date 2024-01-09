05f04fce82847f9df524560455fce347f648131d
BigLofanbl4 <144733912+BigLofanbl4@users.noreply.github.com>
2024-01-10 00:17:21 +0300

    Add files via upload

---
+++ Docker3 - files/3/python-web-app/my-app/app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+
+
+def main():
+    database_url = os.environ["DATABASE_URL"]
+    print(database_url)
+
+
+if __name__ == "__main__":
+    main()
